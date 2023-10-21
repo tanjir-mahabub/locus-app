f83b71098e57b09d9eb071d43917904f2f58b1c9
tanjir-mahabub <md.tanjir.mahabub@gmail.com>
2023-10-21 14:44:37 +0600

    query parameter added

--- src/controllers/locusController.ts
+++ src/controllers/locusController.ts
@@ -1,33 +1,23 @@
 import { Request, Response } from 'express';
 import getLocusData from '../models/locusModel';
 
-interface LocusData {
-    [key: string]: string | number;
-}
-
 interface RequestQuery {
     id?: string;
-    assemblyId?: string;
-    regionId?: string;
-    membershipStatus?: string;
+    assembly_id?: string;
+    region_id?: string;
+    membership_status?: string;
 }
 
 const locusController = async (req: Request, res: Response): Promise<void> => {
     try {
-        const { id, assemblyId, regionId, membershipStatus } = req.query as RequestQuery;
-        const data = await getLocusData({ id, assemblyId, regionId, membershipStatus });
 
-        const filteredData = data.filter((item: LocusData) => {
-            if (id !== undefined && item.id !== parseInt(id, 10)) return false;
-            if (assemblyId !== undefined && item.assemblyId !== parseInt(assemblyId, 10)) return false;
-            if (regionId !== undefined && item.regionId !== regionId) return false;
-            if (membershipStatus !== undefined && item.membershipStatus !== membershipStatus) return false;
+        /**
+         * Requested query checking
+         */
+        const { id, assembly_id, region_id, membership_status } = req.query as RequestQuery;
+        const data = await getLocusData({ id, assembly_id, region_id, membership_status });
 
-            return true;
-        });
-
-        //console.log(filteredData);
-        res.json(filteredData);
+        res.json(data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Error retrieving locus data' });
--- src/models/locusModel.ts
+++ src/models/locusModel.ts
@@ -2,9 +2,9 @@ import { DBConnection } from '../config/database';
 
 interface LocusData {
     id?: string;
-    assemblyId?: string;
-    regionId?: string;
-    membershipStatus?: string;
+    assembly_id?: string;
+    region_id?: string;
+    membership_status?: string;
 }
 
 const getLocusData = async (queryParams: LocusData) => {
@@ -19,37 +19,49 @@ const getLocusData = async (queryParams: LocusData) => {
     const queryConditions = [];
     const queryParameters = [];
 
+    /**
+     * Query Paramsmeter Logic
+     */
     if (queryParams.id) {
         queryConditions.push('rl.id = $1');
         queryParameters.push(parseInt(queryParams.id, 10));
     }
 
-    if (queryParams.assemblyId) {
-        queryConditions.push('rl.assemblyId = $' + (queryParameters.length + 1));
-        queryParameters.push(queryParams.assemblyId);
+    if (queryParams.assembly_id) {
+        queryConditions.push('rl.assembly_id = $' + (queryParameters.length + 1));
+        queryParameters.push(queryParams.assembly_id);
         console.log(queryParameters);
     }
 
-    if (queryParams.regionId) {
-        queryConditions.push('rl.regionId = $' + (queryParameters.length + 1));
-        queryParameters.push(parseInt(queryParams.regionId, 10));
+    if (queryParams.region_id) {
+        queryConditions.push('rlm.region_id = $' + (queryParameters.length + 1));
+        queryParameters.push(parseInt(queryParams.region_id, 10));
     }
 
-    if (queryParams.membershipStatus) {
-        queryConditions.push('rlm.membershipStatus = $' + (queryParameters.length + 1));
-        queryParameters.push(queryParams.membershipStatus);
+    if (queryParams.membership_status) {
+        queryConditions.push('rlm.membership_status = $' + (queryParameters.length + 1));
+        queryParameters.push(queryParams.membership_status);
     }
 
+    /**
+     * Condition on Sql based on Query
+     */
     const whereClause = queryConditions.length > 0 ? 'WHERE ' + queryConditions.join(' AND ') : '';
 
+    /**
+     * SQL Schema
+     */
     const sql = `SELECT rl.*, rlm.* FROM rnc_locus rl
         LEFT JOIN rnc_locus_members rlm ON rlm.locus_id = rl.id ${whereClause} LIMIT $${queryParameters.length + 1}`;
 
+    /**
+     * Retrieve data from database
+     */
     try {
         db.connect({ direct: true });
-        const data = await db.any(sql, [...queryParameters, 10]);
+        const data = await db.any(sql, [...queryParameters, 5]);
         db.$pool.end();
-        console.log('data', queryConditions, queryParameters);
+        console.log('data .....................', data, queryParameters, queryConditions);
         return data;
     } catch (error) {
         console.error('Error in getLocusData:', error);
