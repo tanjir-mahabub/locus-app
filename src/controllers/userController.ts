fe1390c1a1613d216ee7abc56d5ff0ff752319f7
tanjir-mahabub <md.tanjir.mahabub@gmail.com>
2023-10-22 21:17:45 +0600

    added user registration login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -49,3 +49,27 @@ export const login = (req: Request, res: Response) => {
     });
 };
 
+// User registration
+export const register = (req: Request, res: Response) => {
+    const { username, password, role } = req.body;
+
+    // Check if the username already exists
+    const existingUser = users.find((u) => u.username === username);
+
+    if (existingUser) {
+        return res.status(400).json({ message: 'Username already taken' });
+    }
+
+    // Hash the password
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+            return res.status(500).json({ message: 'Registration failed' });
+        }
+
+        // Create a new user
+        const user: IUser = { username, password: hashedPassword, role };
+        users.push(user);
+
+        res.status(201).json({ message: 'User registered successfully' });
+    });
+};
--- src/routes/userRouters.ts
+++ src/routes/userRouters.ts
@@ -3,6 +3,9 @@ import * as userController from '../controllers/userController';
 
 const router = Router();
 
+// Registration route
+router.post('/register', userController.register);
+
 /**
  * Login Route
  */
